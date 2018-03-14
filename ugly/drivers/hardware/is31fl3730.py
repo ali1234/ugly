a025b440a9a197fe014f3f84a7b8ca26404db62a
Alistair Buxton <a.j.buxton@gmail.com>
2018-03-14 20:37:21 +0000

    Add map support to IS31FL3731 driver for matrix 1 and 2.
    This should allow the driver to support any board with this chip,
    including bi-colour boards.

--- ugly/drivers/hardware/is31fl3730.py
+++ ugly/drivers/hardware/is31fl3730.py
@@ -8,24 +8,66 @@ from ugly.drivers.hardware.i2c import Register
 class IS31FL3730(Driver):
 
     mode = Register(0x00)
-    data = Register(0x01)
+    data1 = Register(0x01)
+    data2 = Register(0x0e)
+    update = Register(0x0c)
     brightness = Register(0x19)
+    reset = Register(0xff)
 
-    def __init__(self, rawbuf, bus=1, address=0x60, name='IS31FL3730'):
+    def __init__(self, rawbuf, map1=None, map2=None, bus=1, address=0x60, name='IS31FL3730'):
         super().__init__(rawbuf, 1, name)
         self._bus = smbus.SMBus(bus)
         self._address = address
 
-        self.mode = 0b00000011
+        if map1 is None and map2 is None:
+            raise Exception('Need at least one map.')
+
+        mode = 0
+
+        if map2 is not None:
+            mode |= 0b00001000
+            if map1 is not None:
+                if map1.shape != map2.shape:
+                    raise Exception('Maps must be the same shape.')
+                else:
+                    mode |= 0b00010000
+
+        shape = map2.shape if map1 is None else map1.shape
+        if shape[0] == 8 and shape[1] >= 8 and shape[1] <= 11:
+            mode |= shape[1] - 8
+        else:
+            raise Exception('Map shape not valid for display.')
+
+        self.__map1 = map1
+        self.__map2 = map2
+
+        self.reset = 0
+        self.mode = mode
 
     def show(self):
-        bits = np.pad(self.gammabuf > 0x7f, ((0,3), (0,0), (0,0)), mode='constant')[::-1]
-        outbuf = np.packbits(bits, axis=0).flatten().tolist()
-        outbuf.append(0xff)
-        self.data = outbuf
+        if self.__map1 is not None:
+            bits = (self.gammabuf > 0x7f).flatten().take(self.__map1, mode='clip')
+            self.data1 = np.packbits(bits, axis=0).flatten().tolist()
+
+        if self.__map2 is not None:
+            bits = (self.gammabuf > 0x7f).flatten().take(self.__map2, mode='clip')
+            self.data2 = np.packbits(bits, axis=0).flatten().tolist()
+
+        self.update = 0
 
 
 class ScrollPhat(IS31FL3730):
 
+    map1 = np.array([
+        [ -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1],
+        [ -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1],
+        [ -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1,  -1],
+        [ 44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54],
+        [ 33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43],
+        [ 22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32],
+        [ 11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21],
+        [  0,   1,   2,   3,   4,   5,   6,   7,   8,   9,  10],
+    ], dtype=np.int16)
+
     def __init__(self, bus=1, name='ScrollPhat'):
-        super().__init__(np.zeros((5, 11, 1), dtype=np.uint8), bus=bus, name=name)
+        super().__init__(np.zeros((5, 11, 1), dtype=np.uint8), map1=self.map1, map2=None, bus=bus, name=name)
