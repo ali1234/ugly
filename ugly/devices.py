2389904c140b21b67440c40aaa160b1565c43e8f
Alistair Buxton <a.j.buxton@gmail.com>
2021-09-11 00:17:49 +0100

    Fix the driver import logic
    When a driver is auto, ignore import exceptions. If the driver was
    explicitly specified, raise. Use a context manager for this to avoid
    a lot of repeated code.

--- ugly/devices.py
+++ ugly/devices.py
@@ -8,6 +8,8 @@
 # * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # * GNU General Public License for more details.
 
+import contextlib
+
 from ugly.virtual import Emulator
 
 devices = {}
@@ -17,6 +19,18 @@ def device(f):
 
 def GetDevices():
     return sorted(devices.keys())
+
+
+@contextlib.contextmanager
+def probe(driver):
+    try:
+        yield
+    except ImportError:
+        if driver == "auto":
+            pass
+        else:
+            raise
+
 
 # Factories for actual devices.
 # Real or emulated depending on selected driver.
@@ -32,31 +46,25 @@ def terminal(driver='terminal'):
 def unicornhathd(driver='auto'):
     # tested working
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.unicornhathd import UnicornHatHD
             return UnicornHatHD()
-        except ImportError:
-            pass
     return Emulator(16, 16, 3, 8, driver=driver, name='UnicornHatHD')
 
 @device
 def unicornhat(driver='auto'):
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.ws2812 import WS2812
             return WS2812(8, 8, serpentine=True, name='UnicornHat')
-        except ImportError:
-            pass
     return Emulator(8, 8, 3, 8, driver=driver, name='UnicornHat')
 
 @device
 def unicornphat(driver='auto'):
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.ws2812 import WS2812
             return WS2812(8, 4, serpentine=True, name='UnicornPhat')
-        except ImportError:
-            pass
     return Emulator(8, 4, 3, 8, driver=driver, name='UnicornPhat')
 
 @device
@@ -65,11 +73,9 @@ def cjmcu8x8(driver='auto'):
     Generic AliExpress 8x8 WS2812 Panel
     """
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.ws2812 import WS2812
             return WS2812(8, 8, serpentine=False, name='CJMCU 8x8')
-        except ImportError:
-            raise
     return Emulator(8, 8, 3, 8, driver=driver, name='CJMCU 8x8')
 
 @device
@@ -78,53 +84,41 @@ def homebrew(driver='auto'):
     DIY holtek panel, 15x7
     """
     if driver == 'native' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.hardware.ht16k33 import Homebrew
             return Homebrew()
-        except ImportError:
-            raise
     return Emulator(15, 7, 1, 1, driver=driver, name='Homebrew')
 
 @device
 def scrollphathd(driver='auto'):
     if driver == 'native' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.hardware.is31fl3731 import ScrollPhatHD
             return ScrollPhatHD()
-        except ImportError:
-            raise
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.scrollphathd import ScrollPhatHD
             return ScrollPhatHD()
-        except ImportError:
-            pass
     return Emulator(17, 7, 1, 8, driver=driver, name='ScrollPhatHD')
 
 @device
 def scrollphat(driver='auto'):
     if driver == 'native' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.hardware.is31fl3730 import ScrollPhat
             return ScrollPhat()
-        except ImportError:
-            raise
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.scrollphat import ScrollPhat
             return ScrollPhat()
-        except ImportError:
-            pass
     return Emulator(11, 5, 1, 1, driver=driver, name='ScrollPhat')
 
 @device
 def blinkt(driver='auto'):
     if driver == 'legacy' or driver == 'auto':
-        try:
+        with probe(driver):
             from ugly.drivers.legacy.blinkt import Blinkt
             return Blinkt()
-        except ImportError:
-            pass
     return Emulator(8, 1, 3, 8, driver=driver, name='Blinkt')
 
 def Display(device, driver='auto'):
